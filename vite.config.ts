ea59a97684ae115fb0e5e97d8af18401ce1d81fc
adameubanks <theadameubanks@gmail.com>
2025-07-24 15:41:44 -0600

    ready to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/embeddings-over-time/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000,
